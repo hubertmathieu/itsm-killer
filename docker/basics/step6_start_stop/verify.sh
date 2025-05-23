30bbd0737fc8ac526dd807ec12169122fd4da658
hubertmathieu <hmathieu53@gmail.com>
2025-05-23 13:11:11 +0200

    step-6-verif

--- docker/basics/step6_start_stop/verify.sh
+++ docker/basics/step6_start_stop/verify.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-#docker run -it --rm --name hello-world alpine:latest "echo Hello World"
+set -e
+
+docker inspect nginx_test --format={{.State.Status}} | grep running > /dev/null 2>&1
