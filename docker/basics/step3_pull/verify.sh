58f85846523b605ce3727b8377566d73172ae6bb
hubertmathieu <hmathieu53@gmail.com>
2025-05-23 09:13:02 +0200

    step-3-verif

--- docker/basics/step3_pull/verify.sh
+++ docker/basics/step3_pull/verify.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-#docker run -it --rm --name hello-world alpine:latest "echo Hello World"
+if docker image inspect nginx:latest --format='{{.RepoTags}}' > /dev/null 2>&1; then
+  echo "✅ nginx image found. You have successfully pulled it."
+  exit 0
+else
+  echo "❌ nginx image not found. Please run: docker pull docker.io/library/nginx"
+  exit 1
+fi
