735928d66af97015d07dc09fc8b37681c94176fe
hubertmathieu <hmathieu53@gmail.com>
2025-05-23 09:13:02 +0200

    step-3-verif

--- docker/basics/step3_pull/verify.sh
+++ docker/basics/step3_pull/verify.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-#docker run -it --rm --name hello-world alpine:latest "echo Hello World"
+# script will exit as soon as a command fails
+set -e 
+
+# > /dev/null 2>&1: Hide both the output and any errors (doesn’t show anything to the user)
+docker image inspect nginx:latest --format='{{.RepoTags}}' > /dev/null 2>&1
