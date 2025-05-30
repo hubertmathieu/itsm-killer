6d6d4f8eb629a16f72306adfad5d60fb22344502
InRiPa <InRiPa@users.noreply.github.com>
2025-05-30 11:34:00 +0200

    Update verify.sh
    Check if ENV variable exists and if it contains a valid IPv4 address.

--- kubernetes/kub_first_deployment/step2_create_deployment/verify.sh
+++ kubernetes/kub_first_deployment/step2_create_deployment/verify.sh
@@ -2,4 +2,24 @@
 
 set -e
 
-grep "nginx" /tmp/nginx_curl.txt  > /dev/null 2>&1
+is_valid_ip() {
+  local ip="$1"
+  # Check if IP address is valid IPv4
+  if [[ "$ip" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    IFS='.' read -r i1 i2 i3 i4 <<<"$ip"
+    # Check if each octet is between 0 and 255
+    if ((i1 >= 0 && i1 <= 255)) && ((i2 >= 0 && i2 <= 255)) && ((i3 >= 0 && i3 <= 255)) && ((i4 >= 0 && i4 <= 255)); then
+      return 0 # Valid IP
+    fi
+  fi
+  return 1 # Invalid IP
+}
+
+if [ -z "$TEST_ENV" ]; then
+  echo "TEST_ENV is not set."
+  exit 1 # variable is not set
+fi
+
+if ! is_valid_ip "$TEST_ENV"; then
+  exit 2 # not a valid ip address
+fi
